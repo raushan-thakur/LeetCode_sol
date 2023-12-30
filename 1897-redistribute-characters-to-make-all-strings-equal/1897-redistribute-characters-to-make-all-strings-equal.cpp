41951cd58656792c2420b8787d5632ff95c85f66
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2023-12-30 18:23:33 +0530

    Time: 39 ms (5.06%), Space: 11.9 MB (92.22%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+
+        int n = words.size();
+        if(n==1) return true;
+        vector<int> a(26,0);
+        
+        for(int i= 0; i<n; i++){
+            for(int j= 0; j<words[i].size(); j++){
+                int ch = words[i][j] - 'a';
+                a[ch]++;
+            }
+        }
+        
+        for(int i= 0; i<26; i++){
+            if(a[i]) 
+                if((a[i] % n))
+                    return false;
+        }
+        return true;
+    }
+};
