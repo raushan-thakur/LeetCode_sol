0a5121bbfba27f1d8928f4608fa13b137d3fa571
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-02-14 11:45:41 +0530

    Time: 145 ms (77.60%), Space: 143.3 MB (5.30%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans;
+        vector<int> pos ;
+        vector<int> neg ;
+        for(auto i : nums){
+            if(i>0) pos.push_back(i);
+            else neg.push_back(i);
+        }
+        
+        for(int i=0; i<nums.size()/2; i++){
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
+        }
+        
+        return ans;
+    }
+};
