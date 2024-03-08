165b04200767283b91b21f072a729a1c1802eb1c
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-03-08 16:45:08 +0530

    Time: 3 ms (84.14%), Space: 21.8 MB (22.14%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -7,15 +7,17 @@ public:
         for(int i =0; i<n; i++){
             a[nums[i]]++;
         }
-        sort(a.begin(), a.end());
+        int maxi = 0;
         
-        int ans = a[100];
-        int x = a[100];
-        int i = 99;
-        while(a[i] == x){
-            i--;
-            ans+=a[100];
+        for(int i =1; i<101; i++){
+            maxi = max(maxi, a[i]);
         }
-        return ans;
+        
+        int ans = 0;
+        for(int i =1; i<101; i++){
+            if(a[i] == maxi) ans++;
+        }
+
+        return ans*maxi;
     }
 };
