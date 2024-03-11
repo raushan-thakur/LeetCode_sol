23489e7441a705ea958a06d705ed6107b1a73379
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-03-11 11:56:42 +0530

    Time: 3 ms (50.27%), Space: 7.5 MB (66.00%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+        
+         for(int i =0 ; i<s.size(); i++){
+             b[s[i] - 'a']++;
+         }
+        string ans = "";
+        for(int i =0 ; i<order.size(); i++) {
+            if(b[order[i] - 'a']>0){
+                while(b[order[i] - 'a']>0){
+                ans+=order[i];
+                b[order[i] - 'a']--;
+                }
+            }
+        }
+        for(int i =0 ; i<26; i++){
+            if(b[i]>0){
+                while(b[i]>0){
+                    ans+=i+'a';
+                    b[i]--;
+                }
+            }
+        }
+        return ans;
+    }
+};
