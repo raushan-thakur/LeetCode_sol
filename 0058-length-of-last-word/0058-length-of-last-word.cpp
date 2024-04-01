a6715d3e750d2c9d145dda0b935aee607bad3b67
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-04-01 17:00:12 +0530

    Time: 4 ms (47.87%), Space: 7.6 MB (57.50%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        for(int i = s.length()-1; i>=0 ;i --){
-            if(i>=0 && s[i] == ' ') continue;
-            int last = i;
-             while(i>=0 && s[i] != ' ') i--;
-            int ans = last - i;
-            return ans;
+        int len = 0, i = s.length() - 1;
+        while (i >= 0 && s[i] == ' ') i--;
+        while (i >= 0 && s[i] != ' ') {
+            len++;
+            i--;
         }
-        return -1;
+        return len;
     }
 };
