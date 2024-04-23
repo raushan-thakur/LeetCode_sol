49b6ed9567804aa6efc7728e7623ab8512feab81
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-04-24 00:55:48 +0530

    Time: 7 ms (30.71%), Space: 12.8 MB (68.21%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        
-        int m = nums2.size();
-        vector<int> ans;
-        vector<int> vis(1001,0);
-        for(int i=0; i<n;i++){
-            if(!vis[nums1[i]]){
-                vis[nums1[i]]++;
-                for(int j=0; j<m;j++){
-                    if(nums1[i] == nums2[j]){
-                        ans.push_back(nums1[i]);
-                        break;
-                    }
-                }
+        vector<int> cnt(1001, 0);
+        for (auto n: nums1) {
+            if (cnt[n] < 1) cnt[n] += 1;
+        }
+
+        vector<int> res;
+        for (auto n : nums2) {
+            if (cnt[n] == 1) {
+                cnt[n] += 1;
+                res.push_back(n);
             }
         }
-        return ans;
-        
+        return res;
     }
 };
