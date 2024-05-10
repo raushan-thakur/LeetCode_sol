7a970a4751905f447b3b147b9bcb96a0d43c7860
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-05-10 15:17:56 +0530

    Time: 5 ms (54.08%), Space: 9.4 MB (71.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,52 +1,35 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string>& ds){
-        int r = row;
-        int c = col;
-        
-        while(col>=0){
-            if(ds[row][col] == 'Q') return false;
-            col--;
-        }
-        
-        col = c;
-        while(col>=0 && row>=0){
-            if(ds[row][col] == 'Q') return false;
-            col--;
-            row--;
-        }
-        
-        col = c;
-        row = r;
-        while(col>=0 && row<ds.size()){
-            if(ds[row][col] == 'Q') return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-    void solve(int col, int n, vector<string>& ds, vector<vector<string>>& ans){
+    void solve(int col, int n, vector<string>& ds, vector<vector<string>>& ans, vector<int>& rowCheck, vector<int>& upDCheck, vector<int>& downDCheck){
         if(col == n){
             ans.push_back(ds);
             return;
         }
         
         for(int row = 0; row<n; row++){
-            if(isSafe(row, col, ds)){
+            if(!rowCheck[row] && !upDCheck[row+col] && !downDCheck[n-1+col-row]){
+                rowCheck[row] = 1;
+                upDCheck[row+col] = 1;
+                downDCheck[n-1+col-row] = 1;
                 ds[row][col] = 'Q';
-                solve(col+1, n, ds, ans);
+                solve(col+1, n, ds, ans, rowCheck, upDCheck, downDCheck);
                 ds[row][col] = '.';
+                rowCheck[row] = 0;
+                upDCheck[row+col] = 0;
+                downDCheck[n-1+col-row] = 0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        
+        vector<int> rowCheck(n,0);
+        vector<int> upDCheck(2*n-1,0);
+        vector<int> downDCheck(2*n-1,0);
         string s;
         for(int i =0;i <n; i++) s.push_back('.');
         vector<string> ds(n,s);
         
-        solve(0, n, ds, ans);
+        solve(0, n, ds, ans, rowCheck, upDCheck, downDCheck);
         return ans;
     }
 };
