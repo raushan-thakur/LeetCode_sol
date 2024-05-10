e6497a759761026217d131ddd14ac4cd9b8c0d0f
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-05-10 12:20:54 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (88.71%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(vector<int>& nums, vector<int>& vis, vector<int>& ds, vector<vector<int>>& ans){
+        if(ds.size() == nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i = 0; i<nums.size(); i++){
+            if(!vis[i]){
+                vis[i] = 1;
+                ds.push_back(nums[i]);
+                solve(nums, vis, ds, ans);
+                vis[i] = 0;
+                ds.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        vector<int> vis(nums.size(), 0);
+        
+        solve(nums, vis, ds, ans);
+        return ans;
+    }
+};
