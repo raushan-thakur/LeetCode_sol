febe81d85dd4ba4210b6286e7cec884060922fbc
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-05-21 11:17:01 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (81.15%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     void solve(int i, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
-        if(i >= nums.size()){
+        if(i == nums.size()){
             ans.push_back(ds);
             return;
         }
-       solve(i+1, nums, ds, ans);
         ds.push_back(nums[i]);
         solve(i+1, nums, ds, ans);
         ds.pop_back();
+        solve(i+1, nums, ds, ans);
         
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
+      vector<vector<int>>ans;
         vector<int> ds;
         solve(0, nums, ds, ans);
-        
         return ans;
     }
 };
