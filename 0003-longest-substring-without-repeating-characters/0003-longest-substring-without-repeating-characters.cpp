47fb7eb401db75176b943e8270c83653c744ebbc
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-05-22 16:37:09 +0530

    Time: 8 ms (79.00%), Space: 10.5 MB (73.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,15 +6,24 @@ public:
         vector < int > hash(256, -1);
         int l=0;
         int r=0;
+        int la = 0,ra=0;
         
         while(r<n){
             if(hash[s[r]] != -1){
-                    l= max(hash[s[r]]+1, l);
+                    if(hash[s[r]] >=l){
+                        l = hash[s[r]] + 1;
+                    }
             }
-           hash[s[r]] = r;
-            ans = max(ans, r-l+1);
+            hash[s[r]] = r;
+            // ans = max(ans, r-l+1);
+            if(r-l+1 > ans){
+                ans = r-l+1;
+                la = l;
+                ra = r;
+            }
             r++;
         }
+        cout<<s.substr(la, ra-la+1) << " ";
         return ans;
     }
 };
