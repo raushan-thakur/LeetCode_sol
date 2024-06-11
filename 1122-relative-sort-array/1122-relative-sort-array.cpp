ada1ce5aabeca2104fffec8133338fbaa44a7f12
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-06-11 23:05:01 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (41.93%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int, int> mp;
+        for(int i=0; i<arr1.size(); i++){
+            mp[arr1[i]]++;
+        }
+        vector<int> ans;
+        for(int i=0; i<arr2.size(); i++){
+            while(mp[arr2[i]]){
+                ans.push_back(arr2[i]);
+                mp[arr2[i]]--;
+            }
+        }
+    for(auto it = mp.begin(); it != mp.end(); ++it) {
+        cout << it->first << " ";
+        while(it->second > 0) {
+            ans.push_back(it->first);
+            it->second--;
+        }
+    }
+        return ans;
+    }
+};
