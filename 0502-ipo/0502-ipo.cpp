2653de86d309164f2498ffaf5ffd665355259df5
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-06-15 09:39:21 +0530

    Time: 144 ms (54.68%), Space: 81.6 MB (89.17%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pair<int, int>> projects(n);
+        
+        for (int i = 0; i < n; ++i) 
+            projects[i] = {profits[i], capital[i]};
+        
+        sort(projects.begin(), projects.end(), [](pair<int, int> a, pair<int, int> b) {
+            return a.second < b.second;
+        });
+        
+        priority_queue<int> maxProfit;
+        int i = 0;
+        
+        while (k--) {
+            while (i < n && w >= projects[i].second) 
+                maxProfit.push(projects[i++].first);
+
+            if (!maxProfit.empty()) {
+                w += maxProfit.top();
+                maxProfit.pop();
+            }
+        }
+        
+        return w;
+    }
+};
