7491d7ac397198eec1688c8e6d17d1db06d8497a
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-11-09 10:32:51 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (82.82%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,7 +3,7 @@ public:
     int characterReplacement(string s, int k) {
         int ans =0;
         int l=0;
-        int r=0;
+        int r=0;  
         vector<int>hash(26,0);
         int maxLength = 0;
         
@@ -12,10 +12,10 @@ public:
             hash[s[r] -'A']++;
             maxLength = max(maxLength,  hash[s[r] -'A']);
             
-            while((r-l+1)-maxLength>k){
+            if((r-l+1)-maxLength>k){
                 hash[s[l] -'A']--;
-                maxLength=0;
-                for(int i=0; i<26; i++) maxLength = max(maxLength, hash[i]);
+                // maxLength=0;
+                // for(int i=0; i<26; i++) maxLength = max(maxLength, hash[i]);
                 l++;
             }
             if((r-l+1)-maxLength<=k){
