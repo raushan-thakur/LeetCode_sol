9d35f1055d24a3e0c03da2dcc5dd6b9361e0bc9a
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-11-09 09:46:46 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (92.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.length();
-        int ans = 0;
-        vector<int> f(26,0);
-        int maxi = 0;
-        int l =0;
+        int ans =0;
+        int l=0;
         int r=0;
+        vector<int>hash(26,0);
+        int maxLength = 0;
         
-        while(r<n){
-            f[s[r] - 'A']++;
-            maxi = max(maxi, f[s[r] - 'A']);
-                
-            if(r-l+1 - maxi > k){
-                f[s[l] -'A']--;
+        while(r<s.length()){
+            
+            hash[s[r] -'A']++;
+            maxLength = max(maxLength,  hash[s[r] -'A']);
+            
+            while((r-l+1)-maxLength>k){
+                hash[s[l] -'A']--;
+                maxLength=0;
+                for(int i=0; i<26; i++) maxLength = max(maxLength, hash[i]);
                 l++;
             }
-            ans = max(ans, r-l+1);
+            if((r-l+1)-maxLength<=k){
+                ans= max(ans,r-l+1);
+            }
             r++;
         }
-        
         return ans;
     }
 };
